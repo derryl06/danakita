c92e095b1f674834793434afb28ba0ad9eb100b8
derryl06 <derrylyouri7@gmail.com>
2026-02-27 13:35:32 +0700

    Correcting basePath and trailingSlash for production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Menentukan jalur dasar aplikasi agar sesuai dengan nama repository GitHub (danakita)
+  basePath: process.env.NODE_ENV === 'production' ? '/danakita' : '',
+  // Memastikan semua link diakhiri dengan slash (penting untuk GitHub Pages)
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
